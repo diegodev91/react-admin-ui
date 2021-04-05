b5785ef7429a79e30277d0e71bb1e05eb0297e06
Diego Castillo <diego.castillo@tektonlabs.com>
2021-04-04 21:27:04 -0500

    Adding first component!

---
+++ react-admin-ui/src/components/App/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Options from "../Options/Options";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">App test</header>
+      <div>Hi there</div>
+      <Options></Options>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-admin-ui/src/components/Options/Options.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Options() {
+  return <h1>Ola</h1>;
+}
+
+export default Options;
